a68413f74a8e0723f6ec28b9068f1f9f58a4476a
rido <rmpablos@microsoft.com>
2025-02-20 15:22:32 -0800

    Remove node-echo-skill files and add copilotstudio-skill structure

--- samples/complex/copilotstudio-skill/nodejs/src/bot.js
+++ samples/complex/copilotstudio-skill/nodejs/src/bot.js
@@ -2,7 +2,6 @@
 
 import { ActivityHandler, MessageFactory, MsalTokenProvider } from '@microsoft/agents-bot-hosting'
 import { default as pjson } from '../node_modules/@microsoft/agents-bot-hosting/package.json' with { type: "json" }
-import { getUserInfo } from './userGraphClient.js'
 
 export class EchoBot extends ActivityHandler {
     constructor() {
@@ -13,17 +12,6 @@ export class EchoBot extends ActivityHandler {
             await context.sendActivity(MessageFactory.text(replyText, replyText))
             if (text?.includes('version')) {
                 await context.sendActivity(MessageFactory.text('Running on version ' + pjson.version, 'Running on version ' + pjson.version))
-            }
-            if (text?.includes('graph')) {
-                const msalClient = new MsalTokenProvider()
-                const token = await msalClient.getAccessToken(context.adapter.authConfig, 'https://graph.microsoft.com/')
-                const userInfo = await getUserInfo(token, context.activity.from?.aadObjectId)
-                await context.sendActivity(MessageFactory.text(`User: ${ userInfo.$root.givenName } ${ userInfo.$root.surname } ${ userInfo.$root.mail }`))
-                await context.sendActivity(MessageFactory.attachment({
-                    contentType: 'image/png',
-                    contentUrl: userInfo.$root.imageUri,
-                    name: 'User photo'
-                }))
             }
             await next()
         });
