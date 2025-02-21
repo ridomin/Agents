889f624f5f5e03128d23e99f900a92b77c78e6a2
ridomin <ridomin@live.com>
2025-02-21 15:42:38 +0000

    Add npm install step to CI workflow and update ESLint config to ignore node_modules and dist directories

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,7 @@ import tseslint from "typescript-eslint";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  {files: ["**/*.{js,mjs,cjs,ts}"], ignores: ["**/node_modules/**", "**/dist/**"]},
   {languageOptions: { globals: globals.node }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
